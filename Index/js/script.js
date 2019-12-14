70fad1fd2a13c99696e6a3a69c680f0be2ddbba2
kunstissue <55204882+kunstissue@users.noreply.github.com>
2019-12-13 23:54:54 -0500

    Add files via upload

---
+++ Index/js/script.js
@@ -0,0 +1,901 @@
+document.querySelector('.section2').addEventListener('click', function() {
+  document.querySelector('.bg2').style.display = 'flex';
+});
+
+document.querySelector('.closeicon2').addEventListener('click', function(){
+  document.querySelector('.bg2').style.display = 'none';
+});
+
+
+document.querySelector('.section3').addEventListener('click', function() {
+  document.querySelector('.bg3').style.display = 'flex';
+});
+
+document.querySelector('.closeicon3').addEventListener('click', function(){
+  document.querySelector('.bg3').style.display = 'none';
+});
+
+
+document.querySelector('.section5').addEventListener('click', function() {
+  document.querySelector('.bg5').style.display = 'flex';
+});
+
+document.querySelector('.closeicon5').addEventListener('click', function(){
+  document.querySelector('.bg5').style.display = 'none';
+});
+
+document.querySelector('.section4').addEventListener('click', function() {
+  document.querySelector('.bg4').style.display = 'flex';
+});
+
+document.querySelector('.closeicon4').addEventListener('click', function(){
+  document.querySelector('.bg4').style.display = 'none';
+});
+
+$(document).ready(function(){
+	'use strict';
+	console.log('../js/script.js loaded');
+});
+
+
+let animation = anime.timeline({
+	easing: 'linear',
+  direction: 'alternate',
+	loop: true
+});
+
+
+animation.add({
+  targets: '.one',
+  duration: 4000,
+  translateY: 375
+}, ).add({
+  targets: '.two',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.three',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.four',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.five',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.six',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.seven',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.eight',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.nine',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.ten',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.eleven',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.twelve',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.thirteen',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.fourteen',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+
+//end of first & third column components
+
+}, '-=4000').add({
+  targets: '.a',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.b',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.c',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.d',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.e',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.f',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.g',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+
+//end of first column vowels
+
+}, '-=4000').add({
+  targets: '.h',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.i',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.j',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.k',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.l',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.m',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.n',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+
+//end of second column vowels
+
+}, '-=4000').add({
+  targets: '.onehz',
+  duration: 4000,
+  keyframes: [
+    {translateX: 500}
+  ]
+}, '-=4000').add({
+  targets: '.twohz',
+  duration: 4000,
+  keyframes: [
+    {translateX: 500}
+  ]
+}, '-=4000').add({
+  targets: '.threehz',
+  duration: 4000,
+  keyframes: [
+    {translateX: 500}
+  ]
+}, '-=4000').add({
+  targets: '.fourhz',
+  duration: 4000,
+  keyframes: [
+    {translateX: 500}
+  ]
+}, '-=4000').add({
+  targets: '.fivehz',
+  duration: 4000,
+  keyframes: [
+    {translateX: 500}
+  ]
+}, '-=4000').add({
+  targets: '.sixhz',
+  duration: 4000,
+  keyframes: [
+    {translateX: 500}
+  ]
+}, '-=4000').add({
+  targets: '.sevenhz',
+  duration: 4000,
+  keyframes: [
+    {translateX: 500}
+  ]
+}, '-=4000').add({
+  targets: '.eighthz',
+  duration: 4000,
+  keyframes: [
+    {translateX: 500}
+  ]
+}, '-=4000').add({
+  targets: '.ninehz',
+  duration: 4000,
+  keyframes: [
+    {translateX: 500}
+  ]
+}, '-=4000').add({
+  }, '-=4000').add({
+  targets: '.tenhz',
+  duration: 4000,
+  keyframes: [
+    {translateX: 500}
+  ]
+}, '-=4000').add({
+  targets: '.elevenhz',
+  duration: 4000,
+  keyframes: [
+    {translateX: 500}
+  ]
+}, '-=4000').add({
+  targets: '.twelvehz',
+  duration: 4000,
+  keyframes: [
+    {translateX: 500}
+  ]
+}, '-=4000').add({
+    targets: '.thirteenhz',
+  duration: 4000,
+  keyframes: [
+    {translateX: 500}
+  ]
+}, '-=4000').add({
+  targets: '.fourteenhz',
+  duration: 4000,
+  keyframes: [
+    {translateX: 500}
+  ]
+}, '-=4000').add({
+
+
+//end of row of components
+
+  targets: '.o',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.p',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.q',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.r',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.s',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.t',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.u',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+
+//end of third column vowels
+
+  targets: '.one-1',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.two-1',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.three-1',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.four-1',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.five-1',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.six-1',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.seven-1',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+
+// end of third column components
+
+  targets: '.v',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.w',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.x',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.y',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.z',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.zz',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.zzz',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+
+// end of fourth column vowels
+
+  targets: '.eight-1',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.nine-1',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.ten-1',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.eleven-1',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.twelve-1',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.thirteen-1',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.fourteen-1',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+
+// end of fourth column components
+
+  targets: '.one-2',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.two-2',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.three-2',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.four-2',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.five-2',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.six-2',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.seven-2',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+
+// end of fifth column components
+
+  targets: '.a-1',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.a-2',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.a-3',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.a-4',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.a-5',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.a-6',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.a-7',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+
+//end of fifth column vowels//
+
+  targets: '.eight-2',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.nine-2',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.ten-2',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.eleven-2',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.twelve-2',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.thirteen-2',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.fourteen-2',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+
+//end of sixth column components//
+
+  targets: '.b-1',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.b-2',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.b-3',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.b-4',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.b-5',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.b-6',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000').add({
+  targets: '.b-7',
+  duration: 4000,
+  keyframes: [
+    {translateY: 375}
+  ]
+}, '-=4000');
+
+
+//end of sixth column vowels//
+
+document.querySelector('.content-1').addEventListener('click', function() {
+  document.querySelector('.bg-modal-1').style.display = 'flex';
+});
+
+document.querySelector('.close-1').addEventListener('click', function(){
+  document.querySelector('.bg-modal-1').style.display = 'none';
+});
+
+document.querySelector('.content-2').addEventListener('click', function() {
+  document.querySelector('.bg-modal-2').style.display = 'flex';
+});
+
+document.querySelector('.close-2').addEventListener('click', function(){
+  document.querySelector('.bg-modal-2').style.display = 'none';
+});
+
+document.querySelector('.content-3').addEventListener('click', function() {
+  document.querySelector('.bg-modal-3').style.display = 'flex';
+});
+
+document.querySelector('.close-3').addEventListener('click', function(){
+  document.querySelector('.bg-modal-3').style.display = 'none';
+});
+
+document.querySelector('.content-4').addEventListener('click', function() {
+  document.querySelector('.bg-modal-4').style.display = 'flex';
+});
+
+document.querySelector('.close-4').addEventListener('click', function(){
+  document.querySelector('.bg-modal-4').style.display = 'none';
+});
+
+document.querySelector('.content-5').addEventListener('click', function() {
+  document.querySelector('.bg-modal-5').style.display = 'flex';
+});
+
+document.querySelector('.close-5').addEventListener('click', function(){
+  document.querySelector('.bg-modal-5').style.display = 'none';
+});
+
+$('trackbox').html('<audio autoplay><source src="../pronunciation/.mp3"></audio>')
+
+function _(id){
+  return document.getElementById(id);
+}
+function audioApp(){
+  var audio = new Audio();
+  var audio_folder = "pronunciation/";
+  var audio_ext = ".m4a";
+  var audio_index = 1;
+  var is_playing = false;
+  var playingtrack;
+  var trackbox = _("trackbox");
+  var tracks = {
+      "track1":["가 ga", "가"],
+      "track2":['나 na', "나"],
+      "track3":['다 da', "다"],
+      "track4":['라 ra', "라"],
+      "track5":['마 ma', "마"],
+      "track6":["바 ba", "바"],
+      "track7":['사 sa', "사"],
+      "track8":['아 ()a', "아"],
+      "track9":['자 ja', "자"],
+      "track10":['차 cha', "차"],
+      "track11":["카 ka", "카"],
+      "track12":['타 ta', "타"],
+      "track13":['파 pa', "파"],
+      "track14":['하 ha', "하"],
+      "track15":['갸 gya', "갸"],
+      "track15":["거 geo", "거"],
+      "track17":['겨 gyeo', "겨"],
+      "track18":['고 go', "고"],
+      "track19":['교 gyo', "교"],
+      "track20":['구 gu', "구"],
+      "track21":['규 gyu', "규"],
+      "track22":['그 geu', "그"],
+      "track23":["기 gi", "기"],
+      "track24":['개 gae', "개"],
+      "track25":['게 ge', "게"],
+      "track26":['괴 goe', "괴"],
+      "track27":['귀 gwi', "귀"],
+      "track28":["긔 gui", "긔"],
+      "track29":['걔 gyae', "걔"],
+      "track30":['계 gye', "계"],
+      "track31":['과 gwa', "과"],
+      "track32":['궈 gwo', "궈"],
+      "track33":["괘 gwae", "괘"],
+      "track34":['궤 gwe', "궤"],
+      "track35":['각 gak', "각"],
+      "track36":["간 gan", "간"],
+      "track37":['갇 gat', "갇"],
+      "track38":["갈 gal", "갈"],
+      "track39":['감 gam', "감"],
+      "track40":['갑 gap', "갑"],
+      "track41":['갓 gat', "갓"],
+      "track42":['강 gang', "강"],
+      "track43":["갖 gat", "갖"],
+      "track44":['갗 gat', "갗"],
+      "track45":['갘 gak', "갘"],
+      "track46":['같 gat', "같"],
+      "track47":['갚 gap', "갚"],
+      "track48":["갛 gah", "갛"],
+      "track49":['까 kka', "까"],
+      "track50":['따 tta', "따"],
+      "track51":["빠 ppa", "빠"],
+      "track52":['싸 ssa', "싸"],
+      "track53":['짜 jja', "짜"]
+  };
+  for(var track in tracks){
+    var tb = document.createElement("div");
+    var pb = document.createElement("button");
+    var tn = document.createElement("div");
+    tb.className = "trackbar";
+    pb.className = "playbutton";
+    tn.className = "trackname";
+    tn.innerHTML = "" +tracks[track][0];
+    pb.id = tracks[track][1];
+    pb.addEventListener("click", switchTrack);
+    tb.appendChild(pb);
+    tb.appendChild(tn)
+    trackbox.appendChild(tb);
+    audio_index++;
+  }
+  audio.addEventListener("ended",function(){
+      _(playingtrack).style.background = "url(images/play.png)";
+    playingtrack = "";
+    is_playing = false;
+  });
+  function switchTrack(event){
+    if(is_playing){
+        if(playingtrack != event.target.id){
+          is_playing = true;
+        _(playingtrack).style.background = "url(images/play.png)";
+          event.target.style.background = "url(images/pause.png)";
+          audio.src = audio_folder+event.target.id+audio_ext;
+              audio.play();
+      } else {
+          audio.pause();
+          is_playing = false;
+        event.target.style.background = "url(images/play.png)";
+      }
+    } else {
+      is_playing = true;
+      event.target.style.background = "url(images/pause.png)";
+      if(playingtrack != event.target.id){
+        audio.src = audio_folder+event.target.id+audio_ext;
+      }
+          audio.play();
+    }
+    playingtrack = event.target.id;
+  }
+}
+
+window.addEventListener("load", audioApp);
+
+
+function allowDrop(ev){
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev){
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  var nodeCopy = document.getElementById(data).cloneNode(true); /*duplicate the item*/
+  nodeCopy.id = "newId"; /* cannot use same id*/
+  ev.target.appendChild(nodeCopy); /*duplicate the item*/
+}
+
+
+  $("#container").droppable({
+
+    drop: function (event, ui) {
+        console.log("Item was Dropped");
+        $(this).append($(ui.draggable).cloneNode(true));
+    }
+  });
+
+  function myFunction() {
+  alert("This website is only to learn how to write your name in Korean language");
+}
+
+
+var sign = "FIND THE HIDDEN INCANTATION AND PLAY WITH IT";
+
+$("p").mouseover(function(){
+	$(this)
+	.append(sign)
+	.addClass("black");
+})
+	.mouseout(function(){
+	$(this)
+	.empty()
+	.removeClass("black");
+})
+
+$(document).mousemove(function(e){
+	var X = e.pageX;
+	var Y = e.pageY;
+	$(".torch")
+	.css("background", "radial-gradient(circle at "+X+"px "+Y+"px, transparent, #000 60%)")
+})
+
+
+//class(charm) isolated to the document background image
+//this is to give hint where the "real charm" is hidden -> horror movie: typical cliche
+
+
+$(".charm").mouseenter(function(){
+	$(this)
+	.addClass("changecharm");
+})
+	.mouseleave(function(){
+	$(this)
+	.removeClass("changecharm");
+})
+	.click(function(){
+	$(this)
+	.addClass("changecharm");
+
+})
+	$(".charm").dblclick(function(){
+	$(this)
+	.addClass("ghost");
+})
